bb95ab538120c1c84e7ba619baef974d40482bd4
juyeong ko <juko95@me.com>
2023-03-26 21:31:05 +0900

    Feat: [#1] PhotoViewController Navigation 구현
     - 네비게이션 화면 전환 연결
     - DatePicker 구현

--- FindingPhotosProject/View/Controllers/Photo/PhotoDetailViewController.swift
+++ FindingPhotosProject/View/Controllers/Photo/PhotoDetailViewController.swift
@@ -12,6 +12,25 @@ class PhotoDetailViewController: UIViewController {
     
     // MARK: - Properties
     
+    lazy var textField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 0, y: 0, width: 200, height: 30))
+        textField.text = ""
+        textField.borderStyle = UITextField.BorderStyle.roundedRect
+        textField.layer.position = CGPoint(x: self.view.bounds.width/2, y: self.view.bounds.height - 100)
+        return textField
+    }()
+    
+    lazy var datePicker: UIDatePicker = {
+        let datePicker = UIDatePicker(frame: CGRect(x: 0, y: 150, width: self.view.frame.width, height: 200))
+        datePicker.timeZone = NSTimeZone.local
+        datePicker.backgroundColor = .white
+        datePicker.layer.cornerRadius = 5.0
+        
+        datePicker.addTarget(self, action: #selector(onDidChangeDate(sender:)), for: .valueChanged)
+        return datePicker
+    }()
+    
+    
     
     // MARK: - LifeCycle
     
@@ -20,6 +39,10 @@ class PhotoDetailViewController: UIViewController {
         
         configureUI()
         configureNavigation()
+        configureNavigation()
+        
+        self.view.addSubview(self.textField)
+        self.view.addSubview(self.datePicker)
     }
     
     // MARK: - Selectors
@@ -27,19 +50,32 @@ class PhotoDetailViewController: UIViewController {
     @objc func saveButtonTapped(_ sender: Any) {
         print("사진 저장")
     }
-
+    
+    @objc func onDidChangeDate(sender: UIDatePicker) {
+        let dateFormatter: DateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy/MM/dd hh:mm"
+        
+        // 형식에 따라 날짜 가져오기
+        let selectedDate: String = dateFormatter.string(from: sender.date)
+        self.textField.text = selectedDate
+    }
+    
     
     
     // MARK: - Helpers
-
+    
     private func configureUI() {
         view.backgroundColor = .white
     }
     
     private func configureNavigation() {
         let rightButton = UIBarButtonItem(title: "저장", style: .plain, target: self, action: #selector(saveButtonTapped))
-
+        
         navigationItem.rightBarButtonItem = rightButton
     }
-
+    
+    private func configureDataPicker() {
+        self.view.addSubview(self.datePicker)
+    }
+    
 }
--- FindingPhotosProject/View/Controllers/Photo/PhotoViewController.swift
+++ FindingPhotosProject/View/Controllers/Photo/PhotoViewController.swift
@@ -6,19 +6,61 @@
 //
 
 import UIKit
+import PhotosUI
 
 final class PhotoViewController: UIViewController {
     
     // MARK: - Properties
+    
+    var imagePicker = UIImagePickerController()
+    let detailViewController = PhotoDetailViewController()
+//    let navigationItem = UINavigationItem(title: "나의 앨범")
     
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureUI()
+        configureNavigation()
     }
     
-    // MARK: - helpers
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = false
+        navigationController?.navigationBar.barStyle = .default
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func nextButtonTapped(_ sender: Any) {
+        print("DetailVC로 화면 전환")
+        detailViewController.modalPresentationStyle = .fullScreen
+//        self.present(detailViewController, animated: true)
+        self.navigationController?.pushViewController(detailViewController, animated: true)
+    }
+
+    
+    // MARK: - Helpers
+    
+    private func configureUI() {
+        view.backgroundColor = .white
+        navigationController?.navigationBar.tintColor = .tabButtondarkGrey
+        
+    
+    }
+    
+    private func configureNavigation() {
+        navigationItem.title = "나의 앨범"
+        let rightButton = UIBarButtonItem(title: "추가", style: .plain, target: self, action: #selector(nextButtonTapped))
+
+        navigationItem.rightBarButtonItem = rightButton
+//        navigationBar.setItems([navigationItem], animated: true)
+    }
+    
+ 
+    
     
     
     
