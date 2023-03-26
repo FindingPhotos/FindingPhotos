fd07579b6b84d5c8555079509e60064805061e96
Kyle <dpkyle97@gmail.com>
2023-03-26 21:29:37 +0900

    Feat: [#1] MapViewController 화면 UI 구성
    - NMFNaverMapView 사용하여 화면에 추가

--- FindingPhotosProject/View/Controllers/Map/MapViewController.swift
+++ FindingPhotosProject/View/Controllers/Map/MapViewController.swift
@@ -6,21 +6,40 @@
 //
 
 import UIKit
+import NMapsMap
+import SnapKit
 
 final class MapViewController: UIViewController {
     
     // MARK: - Properties
-    
-    
+    private lazy var mapView: NMFNaverMapView = {
+        let mapView = NMFNaverMapView(frame: view.frame)
+        mapView.showZoomControls = false
+        return mapView
+    }()
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .blue
+        setValue()
+        addSubViews()
+        layout()
     }
-    
     // MARK: - helpers
-    
-    
-    
+}
+
+extension MapViewController: LayoutProtocol {
+    func setValue() {
+        view.backgroundColor = .white
+        navigationItem.title = "근처 사진관 찾기"
+    }
+    func addSubViews() {
+        view.addSubview(mapView)
+    }
+    func layout() {
+        mapView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(100)
+            make.bottom.leading.trailing.equalToSuperview()
+        }
+    }
 }
