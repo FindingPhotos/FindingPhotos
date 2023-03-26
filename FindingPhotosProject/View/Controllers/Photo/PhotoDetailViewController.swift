304653d33bfbe882e13b768bf466cd9eed9bb876
juyeong ko <juko95@me.com>
2023-03-26 20:32:14 +0900

    Feat: PhotoDetailViewController Navigation 설정
     - saveButtonTapped 구현

--- FindingPhotosProject/View/Controllers/Photo/PhotoDetailViewController.swift
+++ FindingPhotosProject/View/Controllers/Photo/PhotoDetailViewController.swift
@@ -5,4 +5,41 @@
 //  Created by juyeong koh on 2023/03/26.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class PhotoDetailViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+        configureNavigation()
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func saveButtonTapped(_ sender: Any) {
+        print("사진 저장")
+    }
+
+    
+    
+    // MARK: - Helpers
+
+    private func configureUI() {
+        view.backgroundColor = .white
+    }
+    
+    private func configureNavigation() {
+        let rightButton = UIBarButtonItem(title: "저장", style: .plain, target: self, action: #selector(saveButtonTapped))
+
+        navigationItem.rightBarButtonItem = rightButton
+    }
+
+}
