ac506c2a08f2ab8963a9e54642cabdd2bef81716
juyeong ko <juko95@me.com>
2023-04-01 10:44:28 +0900

    Fix: [#3] 파일 디렉토리 오류

--- FindingPhotosProject/View/Controllers/Photo/PhotoDetailViewController.swift
+++ FindingPhotosProject/View/Controllers/Photo/PhotoDetailViewController.swift
@@ -17,30 +17,21 @@ class PhotoDetailViewController: UIViewController, UINavigationControllerDelegat
     // MARK: - Properties
     
     private let disposeBag = DisposeBag()
-//    private lazy var photoDetailView = PhotoDetailView(frame: self.view.frame)
-    var photoDetailView = PhotoDetailView()
+    private lazy var photoDetailView = PhotoDetailView(frame: self.view.frame)
     private var imagePicker = UIImagePickerController()
     private let photoAuthorizationStatus = PHPhotoLibrary.authorizationStatus()
     
-    
-    // iPhone 내부에 저장된 realm 파일의 주소를 찾아서 알려주는 코드
-    let localRealm = try! Realm()
+
     
     // MARK: - LifeCycle
-    
-    override func loadView() {
-        super.loadView()
-        photoDetailView = PhotoDetailView(frame: self.view.frame)
-        self.view = photoDetailView
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
+        self.view = photoDetailView
         configureUI()
         configureNavigation()
         bindButton()
-        realmSetting()
     }
     
     // MARK: - Selectors
@@ -110,12 +101,6 @@ class PhotoDetailViewController: UIViewController, UINavigationControllerDelegat
 //            .disposed(by: disposeBag)
     }
     
-    
-    private func realmSetting() {
-        // 이 코드를 통해 현재 Realm파일이 저장된 위치의 주소를 확인할 수 있음
-        // 해당 주소를 이용해 Realm파일에 바로 접근하여 저장된 데이터를 확인할 수 있음
-        print("Realm is located at", localRealm.configuration.fileURL!)
-    }
     
     deinit {
         print("deinit!!!")
