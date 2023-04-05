41bd996d154192245655ca08fe92e567b1a324f8
Hyungju LEE <hyungjuice@naver.com>
2023-04-05 14:23:02 +0900

    Feat: [#1] 키보드 자동 높낮이 구현
    - IQKeyboardManager로 구현

--- FindingPhotosProject/AppDelegate.swift
+++ FindingPhotosProject/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         NMFAuthManager.shared().clientId = Bundle.main.naverMapApiKey
-        
+        IQKeyboardManagerObject.setting()
     
         return true
     }
